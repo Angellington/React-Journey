d04b3a7f8709a625c5542ba51b59d6632f92eae1
Minerva <superetro85@gmail.com>
2025-03-25 20:43:53 -0300

    Night 2

---
+++ Season1-Introduce/1-HelloReact/HelloReact/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello React</h1>
+      <p>Please, someday I'll be the hero of  React!!</p>
+      <section className='msg-text'>
+        <div className='char-name'><p>Angel Minerva</p></div>
+        <div className='char-txt'>Olá, senhor! Você gostaria de um abrigo?</div>
+      </section>
+    </>
+  )
+}
+
+export default App
+
+

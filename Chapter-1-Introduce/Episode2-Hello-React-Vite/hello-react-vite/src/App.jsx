02f72c09e96cf80bd8d6f89b04fbafdd53a3c713
Seu Nome de Usuário <superetro85@gmail.com>
2025-03-27 12:07:08 -0500

    erer

--- Chapter-1-Introduce/Episode2-Hello-React-Vite/hello-react-vite/src/App.jsx
+++ Chapter-1-Introduce/Episode2-Hello-React-Vite/hello-react-vite/src/App.jsx
@@ -1,7 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -10,7 +7,7 @@ function App() {
     <>
       <h1>Hello React Vite</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

7807c50ef9080a1598def9e3f1ecf11fe03c86d3
Minerva <superetro85@gmail.com>
2025-03-29 23:53:38 -0300

    Usando assets

--- Season3-Avancando/Avancando/src/App.jsx
+++ Season3-Avancando/Avancando/src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
 
+import Oneshot from './assets/background.png'
+
 function App() {
   return (
     <>
       <h1>Avançando em React</h1>
       <img src='niko.png' alt='niko-segurando-o-sol'></img>
+      <img src={Oneshot} alt="Niko no escuro como robôzinho"/>
     </>
   )
 }

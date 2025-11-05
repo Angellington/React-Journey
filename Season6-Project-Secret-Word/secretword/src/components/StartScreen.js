cf5711776fd8ec99c579d5f59e93abedd11b51cf
Minerva <superetro85@gmail.com>
2025-11-04 22:00:51 -0300

    feat: Secret Wor StartScreen

---
+++ Season6-Project-Secret-Word/secretword/src/components/StartScreen.js
@@ -0,0 +1,11 @@
+import './StartScreen.css'
+
+const StartScreen = () => {
+  return <div className="start">
+    <h1>Secret Word</h1>
+    <p>Clique abaixo para começar a jogar</p>
+    <button>Iniciar</button>
+  </div>
+};
+
+export default StartScreen;

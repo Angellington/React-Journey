78d610c3daf2fa8aa7a3c03132d3c2b4e5da3363
Minerva <superetro85@gmail.com>
2025-11-26 22:21:14 -0300

    Study: Aplying POST with fetchAPI

--- Season-B-IntroduceHTTP/introduction-http/src/App.jsx
+++ Season-B-IntroduceHTTP/introduction-http/src/App.jsx
@@ -5,6 +5,8 @@ import './App.css'
 function App() {
   
   const [products, setProducts] = useState([])
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
   
   const url = "http://localhost:3000/products";
   
@@ -21,18 +23,55 @@ function App() {
     }
     fetchData();
   }, [])
+
+  // Aula 2 - Adição de produtos;
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    const product = {
+      name,
+      price,
+    }
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(product)
+    });
+    
+  }
   
   
-  console.log('products', products)
 
   return (
     <div className="app">
       <h1>Lista de Produtos</h1>
-      {products.map((product) => (
-        <li key={product.id}>
-          {product.name} - R$ {product.price}
-        </li>
-      ))}
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} - R$ {product.price}
+          </li>
+        ))}
+      </ul>
+
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input type="text" value={name} name="name" id="name" onChange={(e) => setName(e.target.value)} />
+          </label>
+          <label>
+            Preço:
+            <input type="text" value={price} name="price" id="price" onChange={(e) => setPrice(e.target.value)} />
+          </label>
+
+          <input type="submit" value="Criar" />
+        </form>
+      </div>
+
+
     </div>
   )
 }

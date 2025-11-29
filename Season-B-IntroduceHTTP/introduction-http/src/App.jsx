6a1a4aa6c31caa91231390d253b034273ccac29c
Minerva <superetro85@gmail.com>
2025-11-29 20:23:20 -0300

    study: Delete Mthod in jsonsever

--- Season-B-IntroduceHTTP/introduction-http/src/App.jsx
+++ Season-B-IntroduceHTTP/introduction-http/src/App.jsx
@@ -1,93 +1,117 @@
-import { useEffect, useState } from 'react';
-import './App.css'
-import { useFetch } from './hooks/useFetch';
+  import { useEffect, useState } from 'react';
+  import './App.css'
+  import { useFetch } from './hooks/useFetch';
 
 
-function App() {
-  
-  const [products, setProducts] = useState([])
-  const [name, setName] = useState("");
-  const [price, setPrice] = useState("");
+  function App() {
+    const [products, setProducts] = useState([])
+    const [name, setName] = useState("");
+    const [price, setPrice] = useState("");
 
-  // 4 - useHook
-  
-  
-  
-  const url = "http://localhost:3000/products";
-  const { data: items } = useFetch(url);
-  
-  // Aula 1 - Resgatando dados;
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const res = await fetch(url)
-  //       const data = await res.json()
-  //       setProducts(data)
-  //     } catch(e) {
-  //       console.error("Deu pau", e)
-  //     }
-  //   }
-  //   fetchData();
-  // }, [])
-
-  // Aula 2 - Adição de produtos;
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+    // 4 - useHook
     
-    const product = {
-      name,
-      price,
+    
+    
+    const url = "http://localhost:3000/products";
+    const { data: items, httpConfig, loading, error } = useFetch(url);
+    
+    // Aula 1 - Resgatando dados;
+    // useEffect(() => {
+    //   const fetchData = async () => {
+    //     try {
+    //       const res = await fetch(url)
+    //       const data = await res.json()
+    //       setProducts(data)
+    //     } catch(e) {
+    //       console.error("Deu pau", e)
+    //     }
+    //   }
+    //   fetchData();
+    // }, [])
+
+    // Aula 2 - Adição de produtos;
+    const handleDelete = (id) => {
+      try{
+        httpConfig(null, "DELETE", id)
+        alert("Deletado com sucesso!")
+      } catch(e){
+        console.error("e", e)
+        alert("Deu erro")
+      }
+
     }
+    const handleSubmit = async (e) => {
+      await e.preventDefault();
+      
+      const product = {
+        name,
+        price,
+      }
 
-    const res = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(product)
-    });
+      // const res = await fetch(url, {
+      //   method: "POST",
+      //   headers: {
+      //     "Content-Type": "application/json"
+      //   },
+      //   body: JSON.stringify(product)
+      // });
 
+      
+      // // Aula 3 - Carregamento Dinâmico;
+      // const addedProduct = await res.json();
+
+      // setProducts((prevProducts) => [...prevProducts, addedProduct])
+
+      //Refatorando POST
+
+
+      httpConfig(product, "POST")
+
+      setName("");
+      setPrice("");
+    }
     
-    // Aula 3 - Carregamento Dinâmico;
-    const addedProduct = await res.json();
+    
 
-    setProducts((prevProducts) => [...prevProducts, addedProduct])
+    return (
+      <div className="app">
+        <h1>Lista de Produtos</h1>
+        {/** Loading */}
+        {loading && <p>Carregando dados</p>}
+        {error && <p>{error}</p>}
+        {!loading && (
+        <ul>
+          {items?.map((product) => (
+            <li key={product.id}>
+              {product.name} - R$ {product.price}
+              <button className='product-button' onClick={() => handleDelete(product.id)}>Remover</button>
+            </li>
+          ))}
+        </ul>
+        )}
 
-    setName("");
-    setPrice("");
-  }
-  
-  
 
-  return (
-    <div className="app">
-      <h1>Lista de Produtos</h1>
-      <ul>
-        {items?.map((product) => (
-          <li key={product.id}>
-            {product.name} - R$ {product.price}
-          </li>
-        ))}
-      </ul>
+        <div className="add-product">
+          <form onSubmit={handleSubmit}>
+            <label>
+              Nome:
+              <input type="text" value={name} name="name" id="name" onChange={(e) => setName(e.target.value)} />
+            </label>
+            <label>
+              Preço:
+              <input type="text" value={price} name="price" id="price" onChange={(e) => setPrice(e.target.value)} />
+            </label>
 
-      <div className="add-product">
-        <form onSubmit={handleSubmit}>
-          <label>
-            Nome:
-            <input type="text" value={name} name="name" id="name" onChange={(e) => setName(e.target.value)} />
-          </label>
-          <label>
-            Preço:
-            <input type="text" value={price} name="price" id="price" onChange={(e) => setPrice(e.target.value)} />
-          </label>
+            {/* State de loading no post */}
+          {loading && (<input type="submit" disabled value="Aguarde" />)}
+          {!loading && (<input type="submit" value="Criar" />)}
+            
+          </form>
+        </div>
 
-          <input type="submit" value="Criar" />
-        </form>
+
       </div>
+    )
+  }
 
-
-    </div>
-  )
-}
-
-export default App
+  export default App
--- Season-B-IntroduceHTTP/introduction-http/src/hooks/useFetch.jsx
+++ Season-B-IntroduceHTTP/introduction-http/src/hooks/useFetch.jsx
@@ -3,22 +3,81 @@ import { useState, useEffect } from "react";
 // 4 - Custom Hook
 
 export const useFetch = (url) => {
-    const [data, setData] = useState(null);
+  const [data, setData] = useState(null);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const res = await fetch(url)
-                const json = await res.json()
-                setData(json)
-            } catch (e) {
-                console.error("Error: ", e)
-            }
-        }
+  // 5 - Refatorando o POST
+  const [config, setConfig] = useState(null);
+  const [method, setMethod] = useState(null);
+  const [callFetch, setCallFetch] = useState(false);
+  const [id, setId] = useState(null)
 
-        fetchData();
 
-    }, [url])
+  // 6 - Loading
+  const [loading, setLoading] = useState(false);
 
-    return { data };
-}
+  // 7 - Error
+  const [error, setError] = useState(null);
+
+  function httpConfig(data, method, id){
+    if(method === "POST"){
+      setConfig({
+        method,
+        headers: {
+          "Content-type": "application/json"
+        },
+        body: JSON.stringify(data)
+      })
+      setMethod(method)
+      setCallFetch(prev => !prev)
+    }
+
+    if(method === "DELETE"){
+      setConfig({
+        method,
+        headers: {
+          "Content-type": "application/json"
+        },
+      })
+      setMethod(method)
+      setId(id)
+      setCallFetch(prev => !prev)
+    }
+  }
+
+  useEffect(() => {
+   const fetchData = async () => {
+      setLoading(true)
+      try{
+        const res = await fetch(url)
+        const json = await res.json()
+        setData(json)
+      } catch (e) {
+        setError("Houve um erro ao carregar os dados")
+      }
+
+      setLoading(false)
+   }
+   fetchData()
+  }, [url, callFetch]);
+
+  useEffect(() => {
+    const httpRequest = async () => {
+      let fetchOptions;
+      if(method === "POST"){  
+        const res = await fetch(...fetchOptions)
+        const json = await res.json();
+  
+      }
+      if(method === "DELETE"){
+        fetchOptions = [`${url + "/" + id}`, config]
+        const res = await fetch(...fetchOptions);
+      }
+      
+      setCallFetch(prev => !prev)
+    }
+
+    httpRequest();
+  }, [config, method, url, id])
+
+  return { data, httpConfig, loading, error};
+};

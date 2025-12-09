75197f5063f9dd40f686bc18f18feedcc484edb3
Minerva <superetro85@gmail.com>
2025-12-08 22:47:22 -0300

    Study: Undestanding more about routes

--- Season8-ReactRouter/react-router/src/App.jsx
+++ Season8-ReactRouter/react-router/src/App.jsx
@@ -1,10 +1,23 @@
 import './App.css'
 
+//1 - Config React Router
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home/home'
+import About from './pages/about/about'
+import Navbar from './components/Navbar'
+
 function App() {
 
   return (
     <>
       <h1>React Router</h1>
+      <BrowserRouter>
+      <Navbar />
+        <Routes>
+          <Route path={"/"} element={<Home />}></Route>
+          <Route path={"/about"} element={<About />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ Season8-ReactRouter/react-router/src/pages/Home/Home.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
+import { useFetch } from '../../hooks/useFetch';
+import "./Home.css"
+
+const Home = () => {
+    const url = 'http://localhost:3000/products'
+
+    const { data: items, loading, error} = useFetch(url)
+
+    if(loading) return <div>Carregando...</div>
+  return (
+    <div>
+        <h1>Products</h1>
+        {error && <p>{error}</p>}
+        <ul className="products">
+            {items && items.map(item => (
+                <li key={item.id}>
+                    <h2>{item.name}</h2>
+                    <p>R${item.price}</p>
+                </li>
+            )) }
+        </ul>
+    </div>
+  )
+}
+
+export default Home

765a6c446a09c7d12b6416683a93cff8fcfb2f91
Minerva <superetro85@gmail.com>
2025-12-09 22:12:21 -0300

    more about routes

--- Season8-ReactRouter/react-router/src/App.jsx
+++ Season8-ReactRouter/react-router/src/App.jsx
@@ -7,6 +7,9 @@ import About from './pages/about/about'
 import Navbar from './components/Navbar'
 import Products from './pages/Product/Products'
 import Info from './pages/Product/info/Info'
+import NotFound from './pages/NotFound/NotFound'
+import SearchForm from './components/SearchForm'
+import Search from './pages/Search/Search'
 
 function App() {
 
@@ -14,12 +17,15 @@ function App() {
     <>
       <h1>React Router</h1>
       <BrowserRouter>
+      <SearchForm />
       <Navbar />
         <Routes>
           <Route path={"/"} element={<Home />}></Route>
+          <Route path={"/search"} element={<Search />}></Route> 
           <Route path={"/about"} element={<About />}></Route>
           <Route path={"/products/:id/"} element={<Products />}></Route>
           <Route path={"/products/:id/info/"} element={<Info />}></Route>
+          <Route path={"*"} element={<NotFound />}></Route>
         </Routes>
       </BrowserRouter>
     </>

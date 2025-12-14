710326032717ad4ee1dad226424ccfbcfdc59082
Minerva <superetro85@gmail.com>
2025-12-13 23:10:14 -0300

    study: search and redirect

--- Season8-ReactRouter/react-router/src/App.jsx
+++ Season8-ReactRouter/react-router/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 
 //1 - Config React Router
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 import Home from './pages/Home/home'
 import About from './pages/about/about'
 import Navbar from './components/Navbar'
@@ -17,15 +17,15 @@ function App() {
     <>
       <h1>React Router</h1>
       <BrowserRouter>
-      <SearchForm />
       <Navbar />
+      <SearchForm />
         <Routes>
           <Route path={"/"} element={<Home />}></Route>
-          <Route path={"/search"} element={<Search />}></Route> 
           <Route path={"/about"} element={<About />}></Route>
           <Route path={"/products/:id/"} element={<Products />}></Route>
           <Route path={"/products/:id/info/"} element={<Info />}></Route>
-          <Route path={"*"} element={<NotFound />}></Route>
+          <Route path={"/search"} element={<Search />}></Route>
+           <Route path={"*"} element={<NotFound />}></Route>
         </Routes>
       </BrowserRouter>
     </>

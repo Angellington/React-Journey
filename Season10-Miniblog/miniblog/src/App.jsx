3953b068413260f6c07acdc9f72643644596c177
Minerva <superetro85@gmail.com>
2025-12-28 10:58:51 -0300

    study: footer and navbar

--- Season10-Miniblog/miniblog/src/App.jsx
+++ Season10-Miniblog/miniblog/src/App.jsx
@@ -2,17 +2,21 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home/Home'
 import About from './pages/About/About'
+import Navbar from './components/navbar'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <>
+    <Navbar />
     <div className="container">
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/about' element={<About />}></Route>
       </Routes>
     </div>
+    <Footer />
     </>
   )
 }

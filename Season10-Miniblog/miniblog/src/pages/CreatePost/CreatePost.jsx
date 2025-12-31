c07035b9d69684cf298e7dde1cf0a4fe45ca198e
Minerva <superetro85@gmail.com>
2025-12-31 04:52:39 -0300

    study: posts and home

--- Season10-Miniblog/miniblog/src/pages/CreatePost/CreatePost.jsx
+++ Season10-Miniblog/miniblog/src/pages/CreatePost/CreatePost.jsx
@@ -1,8 +1,117 @@
-import styles from './CreatePost.module.css'
-const CreatePost = () => {
-  return (
-    <div>CreatePost</div>
-  )
-}
+import styles from "./CreatePost.module.css";
 
-export default CreatePost
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuthValue } from "../../context/AuthContext";
+import { useInsertDocument } from "../../hooks/useInsertDocument";
+
+const CreatePost = () => {
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+  const [body, setBody] = useState("");
+  const [tags, setTags] = useState("");
+  const [formError, setFormError] = useState("");
+
+  const navigate = useNavigate();
+
+  const {user} = useAuthValue()
+
+const { insertDocument, response } = useInsertDocument("posts");
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  setFormError("");
+
+  let error = "";
+
+  try {
+    new URL(image);
+  } catch {
+    error = "A imagem precisa ser uma URL válida.";
+  }
+
+  const tagsArray = tags.split(",").map((tag) => tag.trim().toLowerCase())
+
+  if(!title || !image || !tags || !body){
+    setFormError("Por favor, preencha todos os campos!")
+  }
+
+  if (error) {
+    setFormError(error);
+    return;
+  }
+
+  insertDocument({
+    title,
+    image,
+    body,
+    tagsArray,
+    uid: user.uid,
+    createdBy: user.displayName,
+  });
+  navigate("/")
+};
+
+
+  return (
+    <div className={styles.create_post}>
+      <h2>Criar post</h2>
+      <p>Escreva sobre o que quiser e compartilhe o seu conhecimento</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Título: </span>
+          <input
+            type="text"
+            name="title"
+            required
+            placeholder="Pense num bom título..."
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+          />
+        </label>
+        <label>
+          <span>URL da imagem: </span>
+          <input
+            type="text"
+            name="image"
+            required
+            placeholder="Insira uma imagem que represente o seu post"
+            onChange={(e) => setImage(e.target.value)}
+            value={image}
+          />
+        </label>
+        <label>
+          <span>Conteúdo: </span>
+          <textarea
+            name="body"
+            required
+            placeholder="Insira o conteúdo do post"
+            onChange={(e) => setBody(e.target.value)}
+            value={body}
+          />
+        </label>
+        <label>
+          <span>Tags: </span>
+          <input
+            type="text"
+            name="tags"
+            required
+            placeholder="Insira as tags separadas por vírgulas"
+            onChange={(e) => setTags(e.target.value)}
+            value={tags}
+          />
+        </label>
+           {!response.loading && <button className="btn" type="submit">Cadastrar</button>}
+        {response.loading && <button className="btn" disabled>Aguarde...</button>}
+        {response.error && (
+            <p className="error">{response.error}</p>
+        )}
+        {formError && (
+            <p className="error">{formError}</p>
+        )}
+      </form>
+    </div>
+  );
+};  
+
+export default CreatePost;
--- Season10-Miniblog/miniblog/src/pages/Home/Home.jsx
+++ Season10-Miniblog/miniblog/src/pages/Home/Home.jsx
@@ -1,8 +1,31 @@
-import './Home.module.css'
+import styles from './Home.module.css'
+
+import { useNavigate, Link } from 'react-router-dom'
+import { useState } from 'react'
+
+
 const Home = () => {
+  const [query, setQuery] = useState("")
+  const [posts] = useState([])
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
   return (
-    <div>
-        <h1>Home</h1>
+    <div className={styles.home}>
+      <h2>Veja os nossos posts mais recentes</h2>
+      <form onSubmit={handleSubmit} className={styles.search_form}>
+        <input type="text" placeholder='Ou busque por tags' />
+        <button className='btn.btn_dark' onChange={(e) => setQuery(e.target.value)} value={query}>Pesquisar</button>
+      </form>
+      <div>
+          <h3>Posts...</h3>
+          {posts && posts.length === 0 && (
+            <div className={styles.noposts}>
+              <p>Não foram encontrados posts</p>
+              <Link to="posts/create" className="btn">Criar primeiro post</Link>
+            </div>
+          )}
+      </div>
     </div>
   )
 }

82344addf418917a78b4ef5d777fa26968e52a08
Minerva <superetro85@gmail.com>
2026-01-04 22:52:37 -0300

    study: dashboard page

--- Season10-Miniblog/miniblog/src/pages/Dashboard/Dashboard.jsx
+++ Season10-Miniblog/miniblog/src/pages/Dashboard/Dashboard.jsx
@@ -12,24 +12,40 @@ const Dashboard = () => {
   // posts do usuário
   const { documents: posts, loading, error} = useFetchDocuments("posts", null, uid);
 
+  const onDeleteDocument = (id) => {
+    if(loading){
+      return <p>Carregando...</p>
+    }
+
+  }
+
   return (
-    <div>
+    <div className={styles.dashboard}>
       <h2>Dashboard</h2>
       <p>Gerencie seus posts</p>
       {posts && posts.length === 0 ? (<div className='noposts'>
         <p>Não foram encontrados posts</p>
-        <Link to={'posts/create'}   className="btn">Criar primeiro post</Link>
+        <Link to={'/create'}   className="btn">Criar primeiro post</Link>
       </div>) : (
         <div> 
-          <p>Tem posts!</p>
+          <div className={styles.post_header}>
+            <span>Título</span>
+            <span>Ações</span>
+          </div>
+          {posts && posts.map((post) => (
+            <div key={post.id} className={styles.post_row}>
+              <p>{post.title}</p>
+              <div>
+                <Link to={`/post/${post.id}`} className="btn btn-outline">Ver</Link>
+                <Link to={`/post   /edit/${post.id}`} className='btn btn-outline'>Editar</Link>
+                <button onClick={() => onDeleteDocument(post.id)} className='btn btn-outline danger'>Excluir</button>
+              </div>
+            </div>
+          ))}
         </div>
-      )}
 
-      {posts && posts.map((post) => (
-        <div key={post.title}>
-          <h3>{post.title}</h3>
-        </div>
-      ))}
+)}
+
     </div>
   )
 }
